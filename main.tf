36cc9ce2202383ff83ba67e2df91146973fdb40f
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-17 11:37:31 +1000

    ...

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ resource "google_compute_subnetwork" "custom_subnet" {
   ip_cidr_range = each.value.ip_cidr_range
   region        = each.value.region
   network       = google_compute_network.custom_vpc.id
+  dynamic "secondary_ip_range" {
+    for_each = lookup(each.value, "secondary_ip_ranges", {})
+    content {
+      range_name    = secondary_ip_range.key
+      ip_cidr_range = secondary_ip_range.value
+    }
+  }
 }
 
 resource "google_compute_firewall" "ingress-allow-from-us-west1-all" {
